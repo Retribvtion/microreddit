7dd315f1bb3c8ec7eb02df2f13e7b1897c6bdecd
Retribvtion Rhino <sim2487@gmail.com>
2017-01-31 07:41:34 -0800

    Added posts and comments models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true,
+            length: { maximum: 50, minimum: 3 }
+
+  has_many :posts
+  has_many :comments
 end
